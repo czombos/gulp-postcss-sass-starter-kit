8c9022b66d445716753e2683fdaf0fd9c4450843
czombos <czombostamas@gmail.com>
2015-10-06 21:28:46 +0200

    remove plumber, add variables, separate default and watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,34 @@ var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 var browserSync  = require('browser-sync');
 var prefix       = require('gulp-autoprefixer');
-var plumber      = require('gulp-plumber');
 var uglify       = require('gulp-uglify');
 var rename       = require("gulp-rename");
 var imagemin     = require("gulp-imagemin");
 var pngquant     = require('imagemin-pngquant');
 var cache        = require('gulp-cache');
 
-gulp.task('sass', function() {
-	gulp.src('sass/**/*.sass')
-	.pipe(sourcemaps.init())
-	.pipe(sass({outputStyle: 'compressed'}))
-	.pipe(prefix('> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'))
-	.pipe(sourcemaps.write('./'))
-	.pipe(plumber())
-	.pipe(gulp.dest('css'));
+input  = {
+	'sass': 'sass/**/*.sass',
+	'javascript': 'js/*.js',
+	'images': 'images/original/**/*'
+},
+
+output = {
+	'stylesheets': 'css',
+	'javascript': 'js/min',
+	'images': 'images'
+};
+
+gulp.task('sass', function () {
+	gulp.src(input.sass)
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			includePaths: ['scss'],
+			outputStyle: 'compressed'
+		}))
+		.pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(output.stylesheets));
 });
 
 gulp.task('browser-sync', function() {
@@ -29,31 +42,30 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('scripts', function() {
-	gulp.src('js/*.js')
+	gulp.src(input.javascript)
 	.pipe(uglify())
-	.pipe(rename({
-		dirname: "min",
-		suffix: ".min",
-	}))
-	.pipe(gulp.dest('js'))
+	.pipe(rename({suffix: ".min"}))
+	.pipe(gulp.dest(output.javascript))
 });
 
 gulp.task('images', function () {
-	return gulp.src('images/original/**/*')
+	return gulp.src(input.images)
 	.pipe(cache(imagemin({
 		progressive: true,
 		svgoPlugins: [{removeViewBox: false}],
 		use: [pngquant()]
 	})))
-	.pipe(gulp.dest('images/'));
+	.pipe(gulp.dest(output.images));
 });
 
 gulp.task('clear', function (done) {
 	return cache.clearAll(done);
 });
 
-gulp.task('default', ['clear', 'sass', 'browser-sync', 'scripts', 'images'], function () {
-	gulp.watch('sass/**/*.sass', ['sass']);
-	gulp.watch('js/**/*.js', ['scripts']);
-	gulp.watch('images/*', ['images']);
+gulp.task('watch', function() {
+	gulp.watch(input.sass, ['sass']);
+	gulp.watch(input.javascript, ['scripts']);
+	gulp.watch(input.images, ['images']);
 });
+
+gulp.task('default', ['clear', 'browser-sync', 'watch']);
