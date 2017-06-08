21f34244c341084319eaae80b4681f2d955a88aa
czombos <czombostamas@gmail.com>
2017-06-08 15:51:54 +0200

    cssnano: discard all comment

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('css', function(){
 		autoprefixer({browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']}),
 		mqpacker,
 		pixrem,
-		cssnano
+		cssnano({"discardComments": {"removeAll": true}})
 	];
 	return gulp.src('./sass/style.sass')
 		.pipe(sass().on('error', sass.logError))
